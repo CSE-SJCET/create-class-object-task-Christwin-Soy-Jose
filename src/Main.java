543e120e8bdf09809cf7f1df4ab54a4cc606edce
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2025-07-28 13:12:37 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@
           String name;
           int rollNumber;
           char grade;
-     }
+     public Student(String name, int rollNumber, char grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+     
      void displayDetails(){
           System.out.println("Name:"+name);
           System.out.println("Grade:"+grade);
           System.out.println("rollnumber:"+rollNumber);
      }
-          
+     } 
     public class Main {
       public static void main(String[] args) {
         Student student1= new Student("chris",28,"A");
